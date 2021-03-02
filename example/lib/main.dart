9469efb77987ff9445ed04359a4de87dfa32c672
west007 <tegong007@Mac-mini.local>
2021-03-02 16:36:47 +0800

    更改样例为纯色图

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,10 +54,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<String> images = [
-    "https://image.baidu.com/search/down?tn=download&word=download&ie=utf8&fr=detail&url=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%253A%252F%252Fattach.bbs.miui.com%252Fforum%252F201408%252F07%252F213601f2xz7usscm2z1mjh.jpg%26refer%3Dhttp%253A%252F%252Fattach.bbs.miui.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1611994213%26t%3Dc81b1d241d90032de0697181bbda6727&thumburl=https%3A%2F%2Fss2.bdstatic.com%2F70cFvnSh_Q1YnxGkpoWK1HF6hhy%2Fit%2Fu%3D1588620919%2C359805583%26fm%3D26%26gp%3D0.jpg",
-    "https://ss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/9c16fdfaaf51f3de9ba8ee1194eef01f3a2979a8.jpg",
-    "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa3.att.hudong.com%2F92%2F04%2F01000000000000119090475560392.jpg&refer=http%3A%2F%2Fa3.att.hudong.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613632953&t=6975e3d854fc663b900ff9f980184735",
+  var colors = [
+    Colors.red,
+    Colors.green,
+    Colors.yellow,
   ];
 
   @override
@@ -92,18 +92,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Widget> getImageWidgets() {
     List<Widget> imageWidgets = [];
-    for (var i=0;i<images.length;i++) {
+    for (var i=0;i<colors.length;i++) {
       imageWidgets.add(
           Container(
             clipBehavior: Clip.hardEdge,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(5)),
             ),
-            child: CachedNetworkImage(
-              imageUrl: images[i],
-              placeholder: (context, url) => Image.memory(kTransparentImage),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-              fit: BoxFit.cover,
+            child: Container(
+              color: colors[i],
             ),
           )
       );
